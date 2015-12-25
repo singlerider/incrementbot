3f68ec7e24cb148b0be17a5fe386d25e8d426b50
Shane <haroboy876@gmail.com>
2015-12-25 14:04:20 -0800

    change config allow setting channel at runtime

--- src/config/config_example.py
+++ src/config/config_example.py
@@ -1,18 +1,24 @@
 global config
 
-channel = '#CHANNEL'
+# #### Modify the information in this section
+channel = ""  # Leave blank to specify a channel at runtime
+username = "YOURUSERNAME"
+# get the oauth_password from http://twitchapps.com/tmi/
+oauth_password = "oauth:n8urn832r238273nd82"
+
+
+# #### Do not edit the lines below
+channel = "#" + channel.lstrip("#")  # Leave this line alone
+if channel == "#":
+    channel = "#" + raw_input(
+        "No channel specified in config. Which channel would you like to join? "
+        ).lstrip("#")
 
 config = {
     # details required to login to twitch IRC server
     'server': 'irc.twitch.tv',
     'port': 6667,
-    'username': 'YOURUSERNAME',
-    # get this from http://twitchapps.com/tmi/
-    'oauth_password': 'oauth:sakdaslkdnalksdlklkasd',
-
-    'debug': True,
-    'log_messages': True,
-
+    'username': username,
+    'oauth_password': oauth_password,
     'channels': channel,
-
 }
